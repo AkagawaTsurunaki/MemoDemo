97db229ce40901b94f4f4da98a2fcc010092f472
AkagawaTsurunaki <965145619@qq.com>
2022-06-11 10:25:31 +0800

    Fatal Error Message Box Change

--- src/com/github/akagawatsurunaki/memodemo/handler/MemoDemoHandler.java
+++ src/com/github/akagawatsurunaki/memodemo/handler/MemoDemoHandler.java
@@ -7,7 +7,7 @@ import com.github.akagawatsurunaki.memodemo.exceptions.MemoDemoException;
 public class MemoDemoHandler {
 
 	public static void handle(MemoDemoException e) {
-		JOptionPane.showInternalConfirmDialog(null, e.getMessage(), e.getTitle(), JOptionPane.YES_OPTION);
+		JOptionPane.showMessageDialog(null, e.getMessage(), e.getTitle(),JOptionPane.ERROR_MESSAGE);
 		System.exit(-1);
 	}
 	
